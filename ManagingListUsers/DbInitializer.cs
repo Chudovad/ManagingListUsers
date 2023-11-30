771bfea8c2232c66ebfa8c58ca69c4045f1d380c
Vadim Chudinovich <vadim_chudinovich@mail.ru>
2023-11-30 15:53:19 +0300

    Сlearing the code

--- ManagingListUsers/DbInitializer.cs
+++ ManagingListUsers/DbInitializer.cs
@@ -12,74 +12,8 @@ namespace ManagingListUsers
         }
         public void Initialize()
         {
-            _context.Database.EnsureCreated(); // Проверяет, создана ли БД
+            _context.Database.EnsureCreated();
 
-            // Проверяем, пустая ли база данных
-            if (!_context.UserRoles.Any())
-            {
-                // Добавляем данные
-                var userRole = new List<UserRole>()
-                {
-                    new UserRole()
-                    {
-                        User = new User()
-                        {
-                            Name = "TestUser",
-                            Age = 1,
-                            Email = "test_user@test.com"
-                        },
-                        Role = new Role()
-                        {
-                            RoleName = "User",
-                        }
-                    },
-                    new UserRole()
-                    {
-                        User = new User()
-                        {
-                            Name = "TestAdmin",
-                            Age = 1,
-                            Email = "test_admin@test.com"
-                        },
-                        Role = new Role()
-                        {
-                            RoleName = "Admin",
-                        }
-                    },
-                    new UserRole()
-                    {
-                        User = new User()
-                        {
-                            Name = "TestSupport",
-                            Age = 1,
-                            Email = "test_support@test.com"
-                        },
-                        Role = new Role()
-                        {
-                            RoleName = "Support",
-                        }
-                    },
-                    new UserRole()
-                    {
-                        User = new User()
-                        {
-                            Name = "TestSuperAdmin",
-                            Age = 1,
-                            Email = "test_superAdmin@test.com"
-                        },
-                        Role = new Role()
-                        {
-                            RoleName = "SuperAdmin",
-                        }
-                    }
-                };
-
-                _context.UserRoles.AddRange(userRole);
-                _context.SaveChanges();
-            }
-        }
-        public void SeedDataContext()
-        {
             if (!_context.UserRoles.Any())
             {
                 var userRole = new List<UserRole>()
@@ -137,6 +71,7 @@ namespace ManagingListUsers
                         }
                     }
                 };
+
                 _context.UserRoles.AddRange(userRole);
                 _context.SaveChanges();
             }
